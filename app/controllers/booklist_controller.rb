56956a381bdc438ebdbc9a5d717435e4090baef7
unknown <s1600613@cadpc07.ccedu.local>
2016-10-12 11:55:22 +0900

    first commit

---
+++ app/controllers/booklist_controller.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+class BooklistController < ApplicationController
+
+    def hello2
+        render :text => "こんにちは"
+    end
+
+    def bye2
+        render :text => "さようなら"
+    end
+
+end

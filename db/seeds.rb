a52bea6e53c1ab043f334b1e116d9ee782949fe3
Atnanasi <ys2000pro@gmail.com>
2016-10-19 11:14:04 +0900

    Init datebase(SQLite)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+﻿# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# coding: utf-8
+
+Title.create(name: 'MySQL5 逆引き大全４６０の極意',sales_date: '2010-12-01')
+Title.create(name: 'JavaScript本格入門',sales_date:'2010-11-26')
+Title.create(name: 'プログラムを作ろう！Microsoft ASP.NET 4 入門',sales_date: '2010-11-11')
+Title.create(name: 'プログラムを作ろう！Microsoft Visual C++ 2010 入門',sales_date: '2010-09-06')
+Title.create(name: 'Windows Azure 実践クラウド・プログラミング',sales_date:'2010-07-29')
+Title.create(name: '改訂3版 基礎PHP',sales_date: '2010-06-18')
+Title.create(name: 'Visual Studio 2010スタートアップガイド',sales_date: '2010-06-09')
+Title.create(name: 'PerlフレームワークCatalyst完全入門',sales_date: '2010-05-20')
+Title.create(name: '独習PHP 第2版',sales_date: '2010-04-12')
+Title.create(name: 'title: Apacheポケットリファレンス',sales_date: '2010-04-10')
